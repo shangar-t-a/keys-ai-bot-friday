c27b4029a4d083ad473c25888a90fb715d9af5aa
Keys (Shangar) <shangar202000@gmail.com>
2024-09-21 16:06:28 +0530

    feat: add color for console chat
    Signed-off-by: Keys (Shangar) <shangar202000@gmail.com>

--- friday/main.py
+++ friday/main.py
@@ -1,8 +1,12 @@
 """Friday - AI Personal Assistant. Main module."""
 
+# Standard Library
+from pathlib import Path
+from typing import Literal
+
 # Third Party Library
 from dotenv import load_dotenv
-from pathlib import Path
+from colorama import init, Fore, Style
 
 # Project Library
 from friday.utilities.logger import CustomLogger
@@ -13,6 +17,12 @@ from friday.sdk.generation import GoogleAIGeneration, FridayGenerationError
 
 # Load Environment Variables
 load_dotenv()
+
+# Constants
+CHAT_USER_FOREGROUND_COLOR = Fore.MAGENTA
+CHAT_FRIDAY_FOREGROUND_COLOR = Fore.CYAN
+CHAT_ERROR_FOREGROUND_COLOR = Fore.RED
+CHAT_COLOR_STYLE = Style.BRIGHT
 
 
 class FridayInitializationError(FridayBaseException):
@@ -72,24 +82,50 @@ class Friday:
             ) from err
 
 
+def console_chat_color_formatter(message: str, role: Literal["User", "Friday", "Error"]) -> str:
+    """
+    Format the message for the console chat.
+
+    Args:
+        message (str): Message to format.
+        role (Literal["User", "Friday"]): Role of the message sender.
+
+    Returns:
+        str: Formatted message for the console chat.
+    """
+    if role == "User":
+        return f"{CHAT_USER_FOREGROUND_COLOR}{CHAT_COLOR_STYLE}You: {message}{Style.RESET_ALL}"
+    elif role == "Friday":
+        return f"{CHAT_FRIDAY_FOREGROUND_COLOR}{CHAT_COLOR_STYLE}Friday: {message}{Style.RESET_ALL}"
+    elif role == "Error":
+        return f"{CHAT_ERROR_FOREGROUND_COLOR}{CHAT_COLOR_STYLE}Error: {message}{Style.RESET_ALL}"
+
+
 def main():
     """Main function for Friday AI Personal Assistant."""
     friday = Friday()
+    init()
     response = friday.google_ai_generation.generate_content(prompt="Who are you?")
-    print(response)
+    print(console_chat_color_formatter(response.response.strip(), role="Friday"))
     friday_chat = friday.google_ai_generation.start_new_chat()
 
     while True:
         try:
-            user_input = input("You: ")
+            user_input = input(f"{CHAT_USER_FOREGROUND_COLOR}{CHAT_COLOR_STYLE}You: ")
             try:
                 response = friday.google_ai_generation.send_chat_message(chat=friday_chat, message=user_input)
             except FridayGenerationError as err:
-                print(f"Friday: Failed to send message to the chat session with Friday...")
-                print(f"Error: {err}")
+                print(
+                    console_chat_color_formatter(
+                        "Friday: Failed to send message to the chat session with Friday...", role="Error"
+                    )
+                )
+                print(console_chat_color_formatter(f"Error: {err}", role="Error"))
                 continue
-            print(f"Friday: {response.response.strip()}")
+            print(console_chat_color_formatter(response.response.strip(), role="Friday"))
         except KeyboardInterrupt:
+            response = friday.google_ai_generation.generate_content(prompt="Good Bye!")
+            print("\n" + console_chat_color_formatter(response.response.strip(), role="Friday"))
             break
 
 
